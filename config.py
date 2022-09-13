d013c1e8419933027da17010dc58f38efeadb800
Mehmet <96449723+Mehmet-rg@users.noreply.github.com>
2022-09-13 10:20:09 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ class Config:
     API_ID = int(os.environ.get("API_ID"))
     API_HASH = os.environ.get("API_HASH")
     BOT_OWNER = os.environ.get("BOT_OWNER")
-    BOT_USERNAME = os.environ.get("BOT_USERNAME"
+    BOT_USERNAME = os.environ.get("BOT_USERNAME")
