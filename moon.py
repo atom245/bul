5177760a87be4e308a30f529367f1d147c8af789
Mehmet <96449723+Mehmet-rg@users.noreply.github.com>
2022-10-19 18:52:58 +0300

    Update moon.py

--- moon.py
+++ moon.py
@@ -47,7 +47,7 @@ def bul(_, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f"▶️ **Parça**: [{title[:35]}]({link})\n\n⌚ **Süre**: `{duration}`\n\n•> [𝖬𝗉3 𝖬𝗎𝗓𝗂𝗄 𝖡𝗈𝗍](https://t.me/Mp3MuzikBot) 𝖳𝖺𝗋𝖺𝖿𝗂𝗇𝖽𝖺𝗇 !"
+        rep = f"⚙️ **Parça**: [{title[:35]}]({link})\n⌚ **Süre**: `{duration}`\n\n•> [𝖬𝗉3 𝖬𝗎𝗓𝗂𝗄 𝖡𝗈𝗍](https://t.me/Mp3MuzikBot) 𝖳𝖺𝗋𝖺𝖿𝗂𝗇𝖽𝖺𝗇 !"
         secmul, dur, dur_arr = 1, 0, duration.split(":")
         for i in range(len(dur_arr) - 1, -1, -1):
             dur += int(float(dur_arr[i])) * secmul
