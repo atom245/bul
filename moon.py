c275832f2b357a91bd6dbefa86c0fbba6e31006f
Mehmet <96449723+Mehmet-rg@users.noreply.github.com>
2022-11-21 14:20:23 +0300

    Update moon.py

--- moon.py
+++ moon.py
@@ -179,7 +179,7 @@ def bul(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f"**▷ ᴘᴀʀᴄ̧ᴀ : {title[:35]}\n▷ sᴜ̈ʀᴇ : {duration}\n\n➠ ᴛᴀʟᴇᴘ : {message.from_user.first_name}\n\nㅤ ✘[ㅤ sᴛᴀʀ ʙᴏᴛ ᴋᴀɴᴀʟ ㅤ](https://t.me/StarBotKanal)✘**"
+        rep = f"**▷ ᴘᴀʀᴄ̧ᴀ : {title[:35]}\n▷ sᴜ̈ʀᴇ : {duration}\n\n➠ ᴛᴀʟᴇᴘ : {message.from_user.mention}\n\nㅤ ✘[ㅤ sᴛᴀʀ ʙᴏᴛ ᴋᴀɴᴀʟ ㅤ](https://t.me/StarBotKanal)✘**"
         secmul, dur, dur_arr = 1, 0, duration.split(":")
         for i in range(len(dur_arr) - 1, -1, -1):
             dur += int(dur_arr[i]) * secmul
