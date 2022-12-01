cd526c46d47ab847d29b2b9b0a0dc2b0a04c9594
Mehmet <96449723+Mehmet-rg@users.noreply.github.com>
2022-12-01 13:12:51 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,8 +4,5 @@ class Config:
     BOT_TOKEN = os.environ.get("BOT_TOKEN")
     API_ID = int(os.environ.get("API_ID"))
     API_HASH = os.environ.get("API_HASH")
-    PLAYLIST_NAME = os.environ.get("PLAYLIST_NAME")
-    GROUP = os.environ.get("GROUP") 
-    PLAYLIST_ID = int(os.environ.get("PLAYLIST_ID"))
     BOT_OWNER = os.environ.get("BOT_OWNER")
     BOT_USERNAME = os.environ.get("BOT_USERNAME")
