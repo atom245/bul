7ce1bde3e08dbe8d090d70d817add6984b0258ff
Mehmet <96449723+Mehmet-rg@users.noreply.github.com>
2022-12-22 09:53:38 +0300

    Update moon.py

--- moon.py
+++ moon.py
@@ -72,7 +72,7 @@ def bul(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f"**▷ ᴘᴀʀᴄ̧ᴀ : {title[:35]}\n▷ sᴜ̈ʀᴇ : {duration}\n\n➠ ᴛᴀʟᴇᴘ : {message.from_user.first_name}\n\nㅤ ✘[ㅤ ʀᴏsᴇ ʙᴏᴛ ᴋᴀɴᴀʟ ㅤ](https://t.me/RoseBotKanal)✘**"
+        rep = f"**▷ ᴘᴀʀᴄ̧ᴀ : {title[:35]}\n▷ sᴜ̈ʀᴇ : {duration}\n\n➠ ᴛᴀʟᴇᴘ : {message.from_user.first_name}\n\nㅤ 🌟[ㅤ ʀᴏsᴇ ʙᴏᴛ ᴋᴀɴᴀʟ ㅤ](https://t.me/RoseBotKanal)🌟**"
         secmul, dur, dur_arr = 1, 0, duration.split(":")
         for i in range(len(dur_arr) - 1, -1, -1):
             dur += int(dur_arr[i]) * secmul
@@ -179,7 +179,7 @@ def bul(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f"**▷ ᴘᴀʀᴄ̧ᴀ : {title[:35]}\n▷ sᴜ̈ʀᴇ : {duration}\n\n➠ ᴛᴀʟᴇᴘ : {message.from_user.first_name}\n\nㅤ ✘[ㅤ ʀᴏsᴇ ʙᴏᴛ ᴋᴀɴᴀʟ ㅤ](https://t.me/RoseBotKanal)✘**"
+        rep = f"**▷ ᴘᴀʀᴄ̧ᴀ : {title[:35]}\n▷ sᴜ̈ʀᴇ : {duration}\n\n➠ ᴛᴀʟᴇᴘ : {message.from_user.first_name}\n\nㅤ 🌟[ㅤ ʀᴏsᴇ ʙᴏᴛ ᴋᴀɴᴀʟ ㅤ](https://t.me/RoseBotKanal)🌟**"
         secmul, dur, dur_arr = 1, 0, duration.split(":")
         for i in range(len(dur_arr) - 1, -1, -1):
             dur += int(dur_arr[i]) * secmul
